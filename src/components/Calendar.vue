7eec20fa90a8fa462477a1a1269160e3ba24da91
dbasiev <dbasiev@cloudfactory.ru>
2021-09-17 16:48:19 +0300

    Локализация и формат времени 24 часа

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,154 +1,160 @@
 <template>
-  <div >
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar
-            flat
-        >
-          <v-btn
-              outlined
-              class="mr-4"
-              color="grey darken-2"
-              @click="setToday"
-          >
-            Сегодня
-          </v-btn>
-          <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="prev"
-          >
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
-          </v-btn>
-          <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="next"
-          >
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu
-              bottom
-              right
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                  outlined
-                  color="grey darken-2"
-                  v-bind="attrs"
-                  v-on="on"
-              >
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>День</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Неделя</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Месяц</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
-      <v-sheet height="600">
-        <v-calendar
-            :weekdays="weekdays"
-            ref="calendar"
-            v-model="focus"
-            color="primary"
-            :events="events"
-            :event-color="getEventColor"
-            :type="type"
-            @click:event="showEvent"
-            @click:more="viewDay"
-            @click:date="viewDay"
-            @change="updateRange"
-        ></v-calendar>
-        <v-menu
-            v-model="selectedOpen"
-            :close-on-content-click="false"
-            :activator="selectedElement"
-            offset-x
-        >
-          <v-card
-              color="grey lighten-4"
-              min-width="350px"
+  <div>
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet height="64">
+          <v-toolbar
               flat
           >
-            <v-toolbar
-                :color="selectedEvent.color"
-                dark
+            <v-btn
+                outlined
+                class="mr-4"
+                color="grey darken-2"
+                @click="setToday"
             >
-              <editModal :selectedEvent="selectedEvent"></editModal>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-menu
-                  bottom
-                  offset-y
+              Сегодня
+            </v-btn>
+            <v-btn
+                fab
+                text
+                small
+                color="grey darken-2"
+                @click="prev"
+            >
+              <v-icon small>
+                mdi-chevron-left
+              </v-icon>
+            </v-btn>
+            <v-btn
+                fab
+                text
+                small
+                color="grey darken-2"
+                @click="next"
+            >
+              <v-icon small>
+                mdi-chevron-right
+              </v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-menu
+                bottom
+                right
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    outlined
+                    color="grey darken-2"
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                  <span>{{ typeToLabel[type] }}</span>
+                  <v-icon right>
+                    mdi-menu-down
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="type = 'day'">
+                  <v-list-item-title>День</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'week'">
+                  <v-list-item-title>Неделя</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'month'">
+                  <v-list-item-title>Месяц</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+        </v-sheet>
+        <v-sheet height="600">
+          <v-calendar
+              :weekdays="weekdays"
+              :weekday-format="myDayFormat"
+              :interval-format="intervalFormat"
+              :first-interval= 8
+              :interval-minutes= 60
+              :interval-count= 13
+              ref="calendar"
+              v-model="focus"
+              color="primary"
+              :events="events"
+              :event-color="getEventColor"
+              :type="type"
+              @click:event="showEvent"
+              @click:more="viewDay"
+              @click:date="viewDay"
+              @change="updateRange"
+          ></v-calendar>
+          <v-menu
+              v-model="selectedOpen"
+              :close-on-content-click="false"
+              :activator="selectedElement"
+              offset-x
+          >
+            <v-card
+                color="grey lighten-4"
+                min-width="350px"
+                flat
+            >
+              <v-toolbar
+                  :color="selectedEvent.color"
+                  dark
               >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                      class="ma-2"
-                      v-bind="attrs"
-                      v-on="on"
-                      icon
-                  >
-                    <v-icon>mdi-dots-vertical</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item>
-                   <v-btn
-                       text
-                       small
-                       color="secondary"
-                       @click="deleteEvent"
-                   >Удалить</v-btn>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-toolbar>
-            <v-card-text>
-              <span v-html="'Кабинет ' + selectedEvent.office"></span>
-            </v-card-text>
-            <v-card-text>
-              <span v-html="selectedEvent.listParticipants"></span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                  text
-                  color="secondary"
-                  @click="selectedOpen = false"
-              >
-                Cancel
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
+                <editModal :selectedEvent="selectedEvent"></editModal>
+                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-menu
+                    bottom
+                    offset-y
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        class="ma-2"
+                        v-bind="attrs"
+                        v-on="on"
+                        icon
+                    >
+                      <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list>
+                    <v-list-item>
+                      <v-btn
+                          text
+                          small
+                          color="secondary"
+                          @click="deleteEvent"
+                      >Удалить
+                      </v-btn>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-toolbar>
+              <v-card-text>
+                <span v-html="'Кабинет ' + selectedEvent.office"></span>
+              </v-card-text>
+              <v-card-text>
+                <span v-html="selectedEvent.listParticipants"></span>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                    text
+                    color="secondary"
+                    @click="selectedOpen = false"
+                >
+                  Cancel
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </div>
 
 </template>
@@ -157,21 +163,21 @@
 import EditModal from "./EditModal";
 
 export default {
-  data () {
+  data() {
     return {
       focus: '',
       type: 'month',
       typeToLabel: {
-        month: 'Month',
-        week: 'Week',
-        day: 'Day',
-
+        month: 'Месяц',
+        week: 'Неделя',
+        day: 'День',
       },
       selectedEvent: {},
       selectedElement: null,
       selectedOpen: false,
       events: [],
-      weekdays: [1, 2, 3, 4, 5, 6, 0]
+      weekdays: [1, 2, 3, 4, 5, 6, 0],
+      weekdaysLabels: {1: 'Пн', 2: 'Вт', 3: 'Ср', 4: 'Чт', 5: 'Пт', 6: 'Сб', 0: 'Вс'}
     }
   },
 
@@ -179,28 +185,34 @@ export default {
     editModal: EditModal
   },
 
-  mounted () {
+  mounted() {
     this.$refs.calendar.checkChange()
   },
 
   methods: {
-    viewDay ({ date }) {
+    myDayFormat(d) {
+      return this.weekdaysLabels[d.weekday]
+    },
+    intervalFormat(locale) {
+      return locale.time;
+    },
+    viewDay({date}) {
       this.focus = date
       this.type = 'day'
     },
-    getEventColor (event) {
+    getEventColor(event) {
       return event.color
     },
-    setToday () {
+    setToday() {
       this.focus = ''
     },
-    prev () {
+    prev() {
       this.$refs.calendar.prev()
     },
-    next () {
+    next() {
       this.$refs.calendar.next()
     },
-    showEvent ({ nativeEvent, event }) {
+    showEvent({nativeEvent, event}) {
       const open = () => {
         this.selectedEvent = event
         this.selectedElement = nativeEvent.target
@@ -224,7 +236,7 @@ export default {
     },
   },
   computed: {
-    loading () {
+    loading() {
       return this.$store.getters.loading
     }
   }
